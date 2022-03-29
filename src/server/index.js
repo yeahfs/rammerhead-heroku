262fe808d872d6e30b88f063d4b79c882600093f
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-03-29 08:14:12 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ const RammerheadLogging = require('../classes/RammerheadLogging');
 if (config.reverseProxy) {
     config.getServerInfo = (req) => {
         return { hostname: new URL('http://' + req.headers.host).hostname, port: 443, crossDomainPort: process.env.CD_PORT||config.crossDomainPort, protocol: 'https:' };
-    },
+    }
 }
 
 const logger = new RammerheadLogging({
