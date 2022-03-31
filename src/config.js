a000e35963e03676131cdc659d53d71b061170eb
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-03-31 15:35:10 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,9 +4,6 @@ const fs = require('fs');
 module.exports = {
     //// HOSTING CONFIGURATION ////
 
-    // TRUE IF LOADING BEHIND REVERSE PROXY
-    reverseProxy: true,
-    
     bindingAddress: '0.0.0.0',
     port: process.env.PORT||8080,
     crossDomainPort: process.env.CD_PORT||8081,
@@ -19,20 +16,20 @@ module.exports = {
     // this function's return object will determine how the client url rewriting will work.
     // set them differently from bindingAddress and port if rammerhead is being served
     // from a reverse proxy.
-    getServerInfo: () => ({ hostname: 'localhost', port: process.env.PORT||8080, crossDomainPort: process.env.CD_PORT||8081, protocol: 'http:' }),
+    //getServerInfo: () => ({ hostname: 'localhost', port: 8235, crossDomainPort: 8084, protocol: 'http:' }),
     // example of non-hard-coding the hostname header
-    // getServerInfo: (req) => {
-    //     return { hostname: new URL('http://' + req.headers.host).hostname, port: 443, crossDomainPort: 8443, protocol: 'https: };
-    // },
+    getServerInfo(req) {
+        return { hostname: new URL('http://' + req.headers.host).hostname, port: 443, crossDomainPort: 443, protocol: 'https:'}
+    },
 
     // enforce a password for creating new sessions. set to null to disable
-    password: 'sharkie4life',
+    password: undefined,
 
     // disable or enable localStorage sync (turn off if clients send over huge localStorage data, resulting in huge memory usages)
     disableLocalStorageSync: false,
 
     // restrict sessions to be only used per IP
-    restrictSessionToIP: false,
+    restrictSessionToIP: true,
 
     //// REWRITE HEADER CONFIGURATION ////
 
@@ -70,9 +67,9 @@ module.exports = {
     generatePrefix: (level) => `[${new Date().toISOString()}] [${level.toUpperCase()}] `,
 
     // logger depends on this value
-    getIP: (req) => req.socket.remoteAddress
+    getIP: (req) => req.socket.remoteAddress,
     // use the example below if rammerhead is sitting behind a reverse proxy like nginx
-    // getIP: req => (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim()
+    getIP: req => (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim()
 };
 
 if (fs.existsSync(path.join(__dirname, '../config.js'))) Object.assign(module.exports, require('../config'));
